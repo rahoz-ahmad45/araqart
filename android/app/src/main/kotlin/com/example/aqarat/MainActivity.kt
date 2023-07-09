c556d4e2e46464e64ed57ff661077868dd6007c6
RAHOZ AHMAD <rahozahmad@RAHOZs-MacBook-Pro.local>
2023-07-09 18:49:31 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/aqarat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aqarat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(myapp());
+}
+
+class myapp extends StatefulWidget {
+  const myapp({super.key});
+
+  @override
+  State<myapp> createState() => _myappState();
+}
+
+class _myappState extends State<myapp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
